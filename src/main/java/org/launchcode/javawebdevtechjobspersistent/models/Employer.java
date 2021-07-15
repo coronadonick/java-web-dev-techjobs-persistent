df4838e6fd2058740e44203b975ff573cf18510d
coronadonick <coronadonick18@gmail.com>
2021-07-14 19:29:39 -0500

    Commit and push

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,39 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank (message = "Cannot leave blank")
+    @Size(max = 500, message = "Please shorten input character length")
+    private String location;
+
+    @OneToMany
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,40 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank (message = "Cannot leave blank")
+    @Size(max = 500, message = "Please shorten input character length")
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //getter
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
 }
